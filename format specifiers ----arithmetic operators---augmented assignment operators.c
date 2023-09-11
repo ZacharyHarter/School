a2a057caaf0a253630c34cf0e4da6292ad69e85c
ZacharyHarter <144629109+ZacharyHarter@users.noreply.github.com>
2023-09-11 13:19:03 -0500

    Add files via upload

---
+++ format specifiers ----arithmetic operators---augmented assignment operators.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+
+int main(){
+//FORMAT SPECIFIERS.........................................................................................
+/*
+// format specifier % = defines and formats a type of data to be displayed
+
+// %c = character
+// %s = string (array of characters)
+// %f = float
+// %lf = double
+// %d= integer
+
+// %.1 = decimal precision
+// %1 = minimum field width
+// %- = left align
+
+float item1 = 5.75;
+float item2 = 10.00;
+float item3 = 100.99;
+
+printf("Item 1: $%-10.2f\n", item1);
+printf("Item 2: $%-20.2f\n", item2);
+printf("Item 3: $%5.2f\n", item3);
+
+//FORMAT SPECIFIERS.........................................................................................
+*/
+
+//arithmetic operators.........................................................................................
+
+    //arithmetic operators
+    // + (addition)
+    // - ( subtrtation)
+    // * ( multiplication)
+    // / ( division)
+    // % ( modulus)     gives the remainder of any division as 1 . if no remainder it gives a zero
+    // ++ (increment)   increments by 1
+    // -- ( decrement)      decrements by -1
+
+    /*int x = 5;
+    int y = 2;  
+
+   float z = x / (float) y;
+
+    printf("%0.1f", z);
+*/
+
+ /*
+    int x = 5;
+    int y = 2;  
+
+   int z = x % y;
+
+    printf("%d", z);
+*/
+
+  /* 
+int x = 5;
+int y =2;
+
+x++;
+y--;
+
+    printf("%d", y);
+*/
+
+
+
+//arithmetic operators.........................................................................................
+
+
+
+
+//augmented assignment operators.........................................................................................
+
+// augmented assignment operators = used to replace a statement where an operator
+//                                  takes a variable as one of its arguments and                (kinda like a shortcut)
+//                                  then assigns the result back to the same variable.
+//                                  x = x + 1
+//                                  x+=1
+
+int x = 10;
+
+x+=2;
+
+printf("%d", x);
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
---
+++ user input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+int age;
+
+printf("How old are you\n");
+scanf("%d\n", age);
+
+printf("You are %d years old", age);
+
+return 0;
+}
